15c728bab60fe7efd4b437a189d3bfc955c36592
IbrRashad <rashad.ibrahimbayli@mail.ru>
2022-12-05 21:25:21 +0400

    increasing acces token time

--- src/main/java/com/company/sintra/config/JwtService.java
+++ src/main/java/com/company/sintra/config/JwtService.java
@@ -51,7 +51,7 @@ public class JwtService {
                 .claim("userId", user.getId())
                 .claim("role", user.getAuthorities().stream().map(authority -> authority.getRole().name()).collect(
                         Collectors.toList()))
-                .setExpiration(Date.from(Instant.now().plusSeconds(6000)))
+                .setExpiration(Date.from(Instant.now().plusSeconds(12000)))
                 .setHeader(Map.of("type", "JWT"))
                 .signWith(key, SignatureAlgorithm.HS256);
         return jwtBuilder.compact();
